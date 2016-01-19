133daa0e7a651b77c33a99bcf4330de7d26b64c5
theTwister <lee.foulger.94@gmail.com>
2016-01-19 02:56:14 +0100

    Fixed, No longer runs commands twice.
    It was an unnecessary foreach loop.

--- getMods/Program.cs
+++ getMods/Program.cs
@@ -6,63 +6,60 @@ namespace getMods
     class Program
     {
         static void Main(string[] args)
-    {
+        {
             if (args.Length == 0)
             {
                 getArgs.Program.Help();
             }
-            foreach (string arg in args) 
-            {
-                if (arg.Contains("/") || arg.Contains("-") || arg.Contains("--"))
+            if (args[0].Contains("/") || args[0].Contains("-") || args[0].Contains("--"))
                 {
-                    if (args[0].Contains("h") || args[0].Contains("help"))
-                    {
-                        getArgs.Program.Help();
-                    }
-                    if (args.Length < 1)
-                        return;
-                    else if (args[0].Contains("b") || args[0].Contains("backup"))
-                    {
-                        if (args.Length < 2)
-                        return;
-                        if (args.Length == 3)
-                            getArgs.Program.Backup(args[1], args[2]);
-                        else
-                            getArgs.Program.Backup(args[1], null);
-                    }
-                    else if (args[0].Contains("r") || args[0].Contains("restore"))
-                    {
-                        if (args.Length < 2)
-                            return;
-                        if (args.Length == 3) 
-                            getArgs.Program.Restore(args[1], args[2]);
-                        else
-                            getArgs.Program.Restore(args[1], null);
-                    }
-                    else if (args[0].Contains("p") || args[0].Contains("patch"))
-                    {
-                        if (args.Length < 4)
-                            return;
-                        getArgs.Program.Patch(args[1], args[2], args[3]);
-                    }
-                    else if (args[0].Contains("z") || args[0].Contains("zip"))
-                    {
-                        if (args.Length < 2)
-                            return;
-                        getArgs.Program.Zip(args[1]);
-                    }
-                    else if (args[0].Contains("d") || args[0].Contains("download"))
-                    {
-                        if (args.Length < 3 && args[2].Contains("*.zip"))
-                            return;
-                        getArgs.Program.Download(args[1], args[2]);
-                    }
+                if (args[0].Contains("h") || args[0].Contains("help"))
+                {
+                    getArgs.Program.Help();
+                }
+                if (args.Length < 1)
+                    return;
+                else if (args[0].Contains("b") || args[0].Contains("backup"))
+                {
+                    if (args.Length < 2)
+                    return;
+                    if (args.Length == 3)
+                        getArgs.Program.Backup(args[1], args[2]);
                     else
-                    {
-                        if (args.Length < 2)
-                            return;
-                        getArgs.Program.List(args[0], args[1]);
-                    }
+                        getArgs.Program.Backup(args[1], null);
+                }
+                else if (args[0].Contains("r") || args[0].Contains("restore"))
+                {
+                    if (args.Length < 2)
+                        return;
+                    if (args.Length == 3) 
+                        getArgs.Program.Restore(args[1], args[2]);
+                    else
+                        getArgs.Program.Restore(args[1], null);
+                }
+                else if (args[0].Contains("p") || args[0].Contains("patch"))
+                {
+                    if (args.Length < 4)
+                        return;
+                    getArgs.Program.Patch(args[1], args[2], args[3]);
+                }
+                else if (args[0].Contains("z") || args[0].Contains("zip"))
+                {
+                    if (args.Length < 2)
+                        return;
+                    getArgs.Program.Zip(args[1]);
+                }
+                else if (args[0].Contains("d") || args[0].Contains("download"))
+                {
+                    if (args.Length < 3 && args[2].Contains("*.zip"))
+                        return;
+                    getArgs.Program.Download(args[1], args[2]);
+                }
+                else
+                {
+                    if (args.Length < 2)
+                        return;
+                    getArgs.Program.List(args[0], args[1]);
                 }
             }
         }
